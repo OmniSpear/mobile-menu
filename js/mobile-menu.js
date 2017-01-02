701356ae39bb058f460236aba7ca8c6593cde4d8
Tyler Reed <treed@omnispear.com>
2017-01-02 11:07:48 -0500

    Restructure code, small bug fixes for open top animation, updated README

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,76 +1,79 @@
 // Settings
-
 window.mobileMenu = {
   'resizeTrigger': 768
 };
 
 
-// Menu Top Offsets
-var menuTop = document.querySelector('.openTop .mobileMenu'),
-    offsetHeight = (menuTop === null)?null:menuTop.offsetHeight;
-
-$('#navToggle').click(function(){
-    if (menuTop === null) {return;}
-    if (this.checked) {
-        $('.openTop .contentWrapper').css('top', offsetHeight+'px')
-        $('.openTop .toggleBtn').css('top', offsetHeight + 15 +'px')
-        $('.openTop .bodyOverlay').css('top', offsetHeight+'px')
-    }
-    else {
-        $('.openTop .contentWrapper').css('top', '0')
-        $('.openTop .toggleBtn').css('top', '15' + 'px')
-        $('.openTop .bodyOverlay').css('top', '0')
-    }
-});
-
+// Scroll
 $(window).on('scroll', function() {
     var y_scroll_pos = window.pageYOffset;
     var scroll_pos_test = 0;
 
     if(y_scroll_pos > scroll_pos_test) {
-        $('.navToggleTop').css('top', '5' + 'px')
+        $('.nav-toggle-top').css('top', '5' + 'px')
     }
 });
 
 // Menu Toggles
+
+/* Menu Top Offsets */
+var menuTop = document.querySelector('.open-top .omni-mobile-menu'),
+    offsetHeight = (menuTop === null)?null:menuTop.offsetHeight;
+
 function closeMenu () {
-    $(".toggleBtn").removeClass("toggled");
-    $(".contentWrapper").removeClass("open-sidebar");
-    $(".bodyOverlay").removeClass("overlayClear");
-    document.querySelector('.contentWrapper').removeEventListener('click', toggleMenu);
+    $(".toggle-btn").removeClass("toggled");
+    $(".content-wrapper").removeClass("open-sidebar");
+    $(".body-overlay").removeClass("overlay-clear");
+    document.querySelector('.content-wrapper').removeEventListener('click', toggleMenu);
 }
 
 function toggleMenu () {
-    var container = document.querySelector('.contentWrapper'),
-        toggleBtn = document.querySelector('.toggleBtn'),
-        bodyOverlay = document.querySelector('.bodyOverlay');
+    var container = document.querySelector('.content-wrapper'),
+        toggleBtn = document.querySelector('.toggle-btn'),
+        bodyOverlay = document.querySelector('.body-overlay');
     toggleBtn.classList.toggle('toggled');
     container.classList.toggle('open-sidebar');
-    bodyOverlay.classList.toggle('overlayClear');
+    bodyOverlay.classList.toggle('overlay-clear');
     container.removeEventListener('click', toggleMenu);
 
     if (container.classList.contains('open-sidebar')) {
         container.addEventListener('click', toggleMenu);
     }
+
+    /* Open Top */
+    if ($('body').hasClass('open-top') && container.classList.contains('open-sidebar')) {
+        $('.open-top .content-wrapper').css('top', offsetHeight+'px')
+        $('.open-top .toggle-btn').css('top', offsetHeight + 15 +'px')
+        $('.open-top .body-overlay').css('top', offsetHeight+'px')
+    }
+    else {
+        $('.open-top .content-wrapper').css('top', '0')
+        $('.open-top .toggle-btn').css('top', '15' + 'px')
+        $('.open-top .body-overlay').css('top', '0')
+    }
 }
 
 // Submenus
-$(".dropdownToggle").click(function () {
-
+$(".dropdown-toggle").click(function () {
     $dropdownToggle = $(this);
     $content = $dropdownToggle.next();
-    $content.slideToggle(500, function () {
-        //execute this after slideToggle is done
+    $content.slideToggle(250, function () {
+        if ($('body').hasClass('open-top')) {
+            offsetHeight = menuTop.offsetHeight;
+            $('.open-top .content-wrapper').css('top', offsetHeight+'px')
+            $('.open-top .toggle-btn').css('top', offsetHeight + 15 +'px')
+            $('.open-top .body-overlay').css('top', offsetHeight+'px')
+        }
     });
 
 });
 
 // Close Menu on Links That Start With '#'
-$(".mobileMenu a[href^='#']").click(function() {
-    closeMenu();
+$(".omni-mobile-menu a[href^='#']").click(function() {
+    toggleMenu();
 });
 
-$('.toggleBtn').click(toggleMenu);
+$('.toggle-btn').click(toggleMenu);
 
 // Close Menu on Window Resize
 $(window).on('resize', function(){
@@ -83,8 +86,8 @@ $(window).on('resize', function(){
 // Swipe Functionality
 $(function() {
     if (document.createEvent('TouchEvent')) {
-      $(".openLeft .contentWrapper").swipe( { swipeStatus:leftSwipe, allowPageScroll:"vertical"} );
-      $(".openRight .contentWrapper").swipe( { swipeStatus:rightSwipe, allowPageScroll:"vertical"} );
+      $(".open-left .content-wrapper").swipe( { swipeStatus:leftSwipe, allowPageScroll:"vertical"} );
+      $(".open-right .content-wrapper").swipe( { swipeStatus:rightSwipe, allowPageScroll:"vertical"} );
     }
 
     function leftSwipe(event, phase, direction, distance, duration, fingers)
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ This project was built by [Omnispear](http://www.omnispear.com/).
 
 ## Include Basic Files
 
-Add necessary js files to project before the closing `</body>` tag.
+Add necessary js files to project before the closing `</body>` tag. Be sure you have jQuery linked in your project as well.
     
 ```html
 <script src="js/mobile-menu.js"></script>
@@ -19,14 +19,14 @@ Add CSS to project in the `<head>` section.
 
 ## Basic Markup
 
-Include this markup in the header of your site. This is the basic content for the Menu, including a dropdown, and the hamburger button. Be sure that all of your site content that you want to be pushed when the menu opens is included inside the `.contentWrapper` div.
+Include this markup in the header of your site. This is the basic content for the Menu, including a dropdown and the hamburger button. Be sure that all of your site content that you want to be pushed when the menu opens is included inside the `.content-wrapper` div.
 
 ```html
-    <ul class="mobileMenu" id="mobileMenu">
+    <ul class="omni-mobile-menu" id="omni-mobile-menu">
         <li><a href="#">Home</a></li>
         <li class="dropdown">
-            <a class="dropdownToggle">Dropdown</a>
-            <ul class="dropdownMenu">
+            <a class="dropdown-toggle">Dropdown</a>
+            <ul class="dropdown-menu">
                 <li><a href="#">Sub Item</a></li>
                 <li><a href="#">Another Sub Item</a></li>
                 <li><a href="#">Last Sub Item</a></li>
@@ -37,9 +37,9 @@ Include this markup in the header of your site. This is the basic content for th
         <li><a href="#">Contact</a></li>
     </ul>  
 
-    <input type="checkbox" id="navToggle" class="navToggle" />  
+    <input type="checkbox" id="nav-toggle" class="nav-toggle" />  
     
-    <label for="navToggle" class="toggleBtn" data-toggle=".toggleBtn" data-menu=".contentWrapper">
+    <label for="nav-toggle" class="toggle-btn" data-toggle=".toggle-btn" data-menu=".content-wrapper">
         <div class="hamburger">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -47,7 +47,7 @@ Include this markup in the header of your site. This is the basic content for th
         </div>
     </label>   
 
-    <div class="contentWrapper">
+    <div class="content-wrapper">
         <!--Place Site Content Here-->
     </div>
 ```
@@ -56,7 +56,7 @@ Include this markup in the header of your site. This is the basic content for th
 
 To set the direction the menu opens from, apply a class to the `<body>` tag of your markup. The mobile menu currently supports:
 
-* Left - `.openLeft`
-* Right - `.openRight`
-* Full - `.openFull`
-* Top -  `.openTop` 
+* Left - `.open-left`
+* Right - `.open-right`
+* Full - `.open-full`
+* Top -  `.open-top` 
